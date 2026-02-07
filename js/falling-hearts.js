be25e41edb90987982a664dac93954ed66807c04
Sasha Cheek <sashacheek792@gmail.com>
2026-02-07 14:29:50 -0700

    update for 2026 valentines

--- js/falling-hearts.js
+++ js/falling-hearts.js
@@ -2,10 +2,11 @@ var HeartsBackground = {
   heartHeight: 32,
   heartWidth: 40,
   hearts: [],
-  heartImage: 'https://cdn.pixabay.com/photo/2016/02/04/11/57/heart-1179054_1280.png',
+  heartImage:
+    "https://cdn.pixabay.com/photo/2016/02/04/11/57/heart-1179054_1280.png",
   maxHearts: 15,
   minScale: 1,
-  draw: function() {
+  draw: function () {
     this.setCanvasSize();
     this.ctx.clearRect(0, 0, this.w, this.h);
     for (var i = 0; i < this.hearts.length; i++) {
@@ -15,51 +16,68 @@ var HeartsBackground = {
       heart.image.src = this.heartImage;
       // heart.image.opacity = Math.random();
       this.ctx.globalAlpha = heart.opacity;
-      this.ctx.drawImage (heart.image, heart.x, heart.y, heart.width, heart.height);
+      this.ctx.drawImage(
+        heart.image,
+        heart.x,
+        heart.y,
+        heart.width,
+        heart.height,
+      );
     }
     this.move();
   },
-  move: function() {
-    for(var b = 0; b < this.hearts.length; b++) {
+  move: function () {
+    for (var b = 0; b < this.hearts.length; b++) {
       var heart = this.hearts[b];
       heart.y += heart.ys;
-      if(heart.y > this.h) {
+      if (heart.y > this.h) {
         heart.x = Math.random() * this.w;
         heart.y = -1 * this.heartHeight;
       }
     }
   },
-  setCanvasSize: function() {
-    this.canvas.width = document.documentElement.clientWidth;
-    this.canvas.height = document.documentElement.clientHeight;
+  setCanvasSize: function () {
+    const doc = document.documentElement;
+
+    this.canvas.width = doc.scrollWidth;
+    this.canvas.height = Math.max(
+      doc.scrollHeight,
+      doc.offsetHeight,
+      doc.clientHeight,
+    );
+
     this.w = this.canvas.width;
     this.h = this.canvas.height;
   },
-  initialize: function() {
-    this.canvas = document.getElementById('canvas');
 
-    if(!this.canvas.getContext)
-      return;
+  initialize: function () {
+    this.canvas = document.getElementById("canvas");
+
+    if (!this.canvas.getContext) return;
 
     this.setCanvasSize();
-    this.ctx = this.canvas.getContext('2d');
+    this.ctx = this.canvas.getContext("2d");
 
-    for(var a = 0; a < this.maxHearts; a++) {
-      var scale = (Math.random() * (1 - this.minScale)) + this.minScale;
+    for (var a = 0; a < this.maxHearts; a++) {
+      var scale = Math.random() * (1 - this.minScale) + this.minScale;
       this.hearts.push({
         x: Math.random() * this.w,
         y: Math.random() * this.h,
         ys: Math.random() + 1,
         height: scale * this.heartHeight,
         width: scale * this.heartWidth,
-        opacity: Math.random() + 0.1
+        opacity: Math.random() + 0.1,
       });
     }
-    
+
     setInterval(this.draw.bind(this), 5);
-  }
+  },
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("resize", () => {
+  HeartsBackground.setCanvasSize();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
   HeartsBackground.initialize();
 });
